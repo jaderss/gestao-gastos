9e7bf01cd3c35942a32c02cae60e09f00aec1089
Jader da Silva Santos <jader.rs@gmail.com>
2021-08-03 15:13:09 -0300

    Initial commit

---
+++ src/test/java/br/com/wipro/challenge/gestaogastos/GestaoGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wipro.challenge.gestaogastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoGastosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
