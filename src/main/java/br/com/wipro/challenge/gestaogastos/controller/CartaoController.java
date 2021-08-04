265c8a5206cc9cef79a6e5e43e854f0626a87c90
Jader da Silva Santos <jader.rs@gmail.com>
2021-08-04 01:22:23 -0300

    Criaçao de controller de teste para cartões.

---
+++ src/main/java/br/com/wipro/challenge/gestaogastos/controller/CartaoController.java
@@ -0,0 +1,24 @@
+package br.com.wipro.challenge.gestaogastos.controller;
+
+import br.com.wipro.challenge.gestaogastos.domain.Cartao;
+import br.com.wipro.challenge.gestaogastos.repository.CartaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cartoes")
+@RequiredArgsConstructor
+public class CartaoController {
+
+    private final CartaoRepository repository;
+
+    @GetMapping
+    public List<Cartao> getCartaoPorCPF(@RequestParam String cpf) {
+        return repository.findByCpfTitular(cpf);
+    }
+}
